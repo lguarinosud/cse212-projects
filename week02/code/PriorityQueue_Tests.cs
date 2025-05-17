bc08bdee1536de0e95d949b5c479647210e14708
Leonardo Guarino <leonardo.guarino@redcentricplc.com>
2025-05-17 15:09:09 +0100

    W02 Code: Queues

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,24 +6,68 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Enqueue multiple items with the same priority and check that they are dequeued in the order they were added.
+    // Expected Result: First out: "A", second out: "B", third out: "C". This will be compared when dequieued and the equal assertion
+    // needs to be true.
+    // Defect(s) Found: In first enqueued, Expected A but got B. A few issues:
+    // 1. In the Dequeue method, the loop was starting from 1 instead of 0, which caused it to skip the first item.
+    // 2. The condition in the loop was checking for less or equal than instead of less  causing to carry over, if equal priority, the last item added instead of first.
+    // 3. A line was missing to remove the item from the queue. 
+    // 4. Within the for loop, the iteration was from 0 until queue.Count -1 was less than index, which made it skip the last item. 
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("A", 5);
+        priorityQueue.Enqueue("B", 5);
+        priorityQueue.Enqueue("C", 5);
+
+        Assert.AreEqual("A", priorityQueue.Dequeue());
+        Assert.AreEqual("B", priorityQueue.Dequeue());
+        Assert.AreEqual("C", priorityQueue.Dequeue());
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: Enqueue multiple items with different priorities and check that they are dequeued in the order of priority.
+    // Expected Result: First out: "C", second out: "B", third out: "A". This will be compared when dequieued and the equal assertion
+    // needs to be true.
+    // Defect(s) Found: In first enqueued, it Expected C but got B. 
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("A", 1);
+        priorityQueue.Enqueue("B", 2);
+        priorityQueue.Enqueue("C", 3);
+
+
+        Assert.AreEqual("C", priorityQueue.Dequeue());
+        Assert.AreEqual("B", priorityQueue.Dequeue());
+        Assert.AreEqual("A", priorityQueue.Dequeue());
+
     }
 
     // Add more test cases as needed below.
+
+    [TestMethod]
+    // Scenario: Enqueue mutiple items with different priorities. Two of them have the same priority.
+    // Expected Result: First out: "C", second out: "B", third out: "A". This will be compared when dequieued and the equal assertion
+    // needs to be true.
+    // Defect(s) Found: In second enqueued, it Expected B but got C. 
+    public void TestPriorityQueue_3()
+    {
+        var priorityQueue = new PriorityQueue();
+
+        priorityQueue.Enqueue("A", 1);
+        priorityQueue.Enqueue("B", 2);
+        priorityQueue.Enqueue("C", 3);
+        priorityQueue.Enqueue("D", 2);
+
+        Assert.AreEqual("C", priorityQueue.Dequeue());
+        Assert.AreEqual("B", priorityQueue.Dequeue());
+        Assert.AreEqual("D", priorityQueue.Dequeue());
+        Assert.AreEqual("A", priorityQueue.Dequeue());
+
+    }
 }
--- week02/code/TakingTurnsQueue_Tests.cs
+++ week02/code/TakingTurnsQueue_Tests.cs
@@ -11,7 +11,7 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
     // run until the queue is empty
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: In the test, the expected result is not correct.  The last person should be Bob and it was Sue.
     public void TestTakingTurnsQueue_FiniteRepetition()
     {
         var bob = new Person("Bob", 2);
@@ -43,7 +43,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
     // After running 5 times, add George with 3 turns.  Run until the queue is empty.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-    // Defect(s) Found: 
+    // Defect(s) Found: In the test, the expected result is not correct.  It expected  Bob and got Sue instead. The problem was that
+    // in Personqueue class, qhen adding the person to the queue, instead of adding to the back, it was adding to the front. 
     public void TestTakingTurnsQueue_AddPlayerMidway()
     {
         var bob = new Person("Bob", 2);
@@ -85,7 +86,8 @@ public class TakingTurnsQueueTests
     // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
     // Run 10 times.
     // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-    // Defect(s) Found: 
+    // Defect(s) Found: In the test, the expected result is not correct.  It expected  Tim and got Sue instead. Problem was that in getNextGeneration
+    // the logic was not considering the case when the turns were 0 or less.  It was only checking for greater than 1.
     public void TestTakingTurnsQueue_ForeverZero()
     {
         var timTurns = 0;
